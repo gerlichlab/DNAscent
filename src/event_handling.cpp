ca838b87d49e96248bc4378c95a9d080690a0c75
Pfuderer <plp27@cam.ac.uk>
2023-03-17 14:08:10 +0000

    debugging segmentation fault error attempt 2

--- src/event_handling.cpp
+++ src/event_handling.cpp
@@ -654,9 +654,8 @@ void normaliseEvents( read &r, bool bulkFast5 ){
 	free(et.event);
 
 	*/
-	event_table et = detect_events(&(r.raw)[0], (r.raw).size(), event_detection_defaults);
-	assert(et.n > 0);
-	r.normalisedEvents.reserve(et.n);
+
+	r.normalisedEvents.reserve((r.raw).size());
 	r.normalisedEvents = r.raw;
 
 	//testing - print the event and the raw signals that were used to make it
